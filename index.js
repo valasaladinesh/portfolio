146508e6ea24527f154fa5c2bbb6fab1b71a79f3
valasaladinesh <valasaladinesh@gmail.com>
2021-07-12 17:33:22 +0530

    added tech slider and improved responsivness

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 
+// scroll fucntion
+function navMenu(){
+    var navBar =  document.querySelector('.navbar-sticky');
+    console.log("hey")
+
+    window.onscroll = function(){
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 120){
+            navBar.classList.add("navbar-sticky-moved-up");
+        }else{
+            navBar.classList.remove("navbar-sticky-moved-up");
+        }
+        if(scroll >= 250){
+            navBar.classList.add("navbar-sticky-transitioned");
+        }else{
+            navBar.classList.remove("navbar-sticky-transitioned");
+        }
+        if(scroll >= 500){
+            navBar.classList.add("navbar-sticky-on");
+        }else{
+            navBar.classList.remove("navbar-sticky-on");
+        }
+    }
+}
+navMenu();
+
 
 function loadDpImages() {
     var images = [
